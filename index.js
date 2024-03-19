866b502861d85d727ebbee32b5f27848bbf68142
tiwaskarkranti20 <153894894+tiwaskarkranti20@users.noreply.github.com>
2024-03-19 10:14:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,144 @@
 
-import React, { useState, useEffect } from 'react';
-import { BarChart, Bar, XAxis, YAxis, Legend, ResponsiveContainer,CartesianGrid } from 'recharts';
-import axios from 'axios';
+
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Statistics from '../Statistics'
+import BarChartComponent from '../BarChart'
 import './index.css'
 
-const BarChartComponent = (props) => {
-    const [barChartData, setBarChartData] = useState([]);
-  const { selectedMonth } = props;
 
-  useEffect(() => {
-    const getBarChartData = async () => {
-      try {
-        const response = await axios.get(`https://roxiler-backend-projects.vercel.app/bar-chart?month=${selectedMonth}`);
-        setBarChartData(response.data);
-      } catch (error) {
-        console.error('Error fetching bar chart data:', error);
-      }
-    };
+const months = [
+    {
+        name: "January",
+    },
+    {
+        name: "February",
+    },
+    {
+        name: "March",
+    },
+    {
+        name: "April",
+    },
+    {
+        name: "May",
+    },
+    {
+        name: "June",
+    },
+    {
+        name: "July",
+    },
+    {
+        name: "August",
+    },
+    {
+        name: "September",
+    },
+    {
+        name: "October",
+    },
+    {
+        name: "November",
+    },
+    {
+        name: "December",
+    },
+]
 
-    getBarChartData();
-  }, [selectedMonth]);
+const Transactions = () => {
+    const [transactionList, setTransactionList] = useState([])
+    const [selectedMonth, setSelectedMonth] = useState(months[2].name)
+    const [searchInput, setSearchInput] = useState('')
+    const [page, setPage] = useState(1)
 
-  const DataFormatter = (number) => {
-    if (number > 1000) {
-      return `${(number / 1000).toString()}k`;
-    }
-    return number.toString();
-  };
 
-  return (
-<div className='barchart-container'>
-    <h2>Bar Chart Status - {selectedMonth}</h2>
-<ResponsiveContainer width="100%" height={300}>
-      <BarChart data={barChartData} margin={{ top: 5 }}>
-      <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="priceRange"
-          tick={{ stroke: 'gray', strokeWidth: 1 }}
-        />
+    useEffect(()=>{
+        const getTransactions = async ()=>{
+            const list = await axios.get(`https://roxiler-backend-vijaykumars-projects.vercel.app/transactions?month=${selectedMonth}&page=${page}&search=${searchInput}&perPage=10`)
+            // `https://roxiler-backend-alpha.vercel.app?page=${page}&perPage=10&search=${searchInput}&month=${selectedMonth}`
+            if(list){
+                console.log(list)
+                setTransactionList(list.data.transactions)
+            }
+        }
+        
+        getTransactions()
+    },[page, searchInput, selectedMonth])
 
-        <YAxis
-          tickFormatter={DataFormatter}
-          tick={{ stroke: 'gray', strokeWidth: 1 }}
-        />
-        <Legend wrapperStyle={{ padding: 30 }} />
-        <Bar dataKey="itemCount" name="Number of Items" fill="#37807e" barSize="20%" />
-      </BarChart>
-    </ResponsiveContainer>
-</div>
-  );
-};
+console.log(selectedMonth)
 
-export default BarChartComponent;
+    return(
+        <>
+    <div className='main-container'>
+    <div className='first-container'>
+        <h3 width="20px">Transaction
+            Dashboard
+        </h3>
+    </div>
+    <div className='second-container'>
+        <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className='input-element' type="search" placeholder='Search transaction' />
+        <select  value={selectedMonth} onChange={(e)=> setSelectedMonth(e.target.value)} className='dropdown-list'>
+            {months.map(o=> <option  className='selector-element' key={o.name} value={o.name}>{o.name}</option>)}
+        </select>
+    </div>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Category</th>
+                <th>Sold</th>
+                <th>Image</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                transactionList.map((o)=>{
+                    const {category,
+                        // dateOfSale,
+                        description,
+                        id,
+                        // image,
+                        price,
+                        sold,
+                        title} = o
+                    return(
+                        <tr key={o.id}>
+                        <td>{id}</td>
+                        <td>{title}</td>
+                        <td>{description}</td>
+                        <td>{price}</td>
+                        <td>{category}</td>
+                        <td>{sold}</td>
+                        {/* <td><img height={'100px'} className='product-image'  src={image}/> */}
+                        {/* </td> */}
+                    </tr>
+                    )
+                })
+            }              
+            </tbody>
+        </table>
+        <div className='last-container'>
+            <p>Page No: {page}</p>
+            <p><span onClick={()=> setPage(prevValue=> prevValue+1)} className='next-button'>Next</span> - <span onClick={()=> setPage(prevValue=> prevValue>1? prevValue-1: prevValue)} className='previous-button'>Previous</span></p>
+            <p>Per Page: 10</p>
+        </div>
+        <div className='statistics-container'>
+            {/* {transactions component} */}
+        </div>
+        <div className='line'>
+            <hr  />
+        </div>
+        
+    </div>
+    <Statistics selectedMonth={selectedMonth} />
+    <BarChartComponent  selectedMonth={selectedMonth} />
+    </>
+
+)
+}
+
+export default Transactions;
